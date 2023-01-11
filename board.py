bfd1a43c390aae651f67983cfc060daf5c45cc18
Shrey1810 <shreyas.potdar@gmail.com>
2023-01-11 13:44:40 +0100

    board

--- board.py
+++ board.py
@@ -3,6 +3,18 @@
 #     def __init__(self, size):
 #         for i in range(size):
 
+import numpy as np
+
+class Board():
+    def __init__(self, size):
+        self.colomn = size
+        self.row = size
+    
+    def load_board(filename):
+        
+            
+
+
 with open("Rushhour6x6_1.csv", 'r') as f:
     variablearray = []
     for line in f:
