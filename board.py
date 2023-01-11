f216b1dbefc8155105e61b07f6e96ffe7001b6a6
issazouboye <issazouboye@gmail.com>
2023-01-11 13:22:03 +0100

    begin board

--- board.py
+++ board.py
@@ -4,7 +4,12 @@
 #         for i in range(size):
 
 with open("Rushhour6x6_1.csv", 'r') as f:
+    variablearray = []
     for line in f:
-        print(line)
+        line = line.strip()
+        variables = line.split(",")
+        variablearray.append(variables)
+    variablearray.pop(0)
+    print(variablearray)
 
 
