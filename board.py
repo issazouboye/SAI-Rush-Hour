8f5cbe1e257c55cd3946977e633b3296d2f42fbe
Shrey1810 <shreyas.potdar@gmail.com>
2023-01-11 21:46:09 +0100

    innit functie

--- board.py
+++ board.py
@@ -6,14 +6,14 @@
 import numpy as np
 
 class Board():
-    def __init__(self, size):
+    def __init__(self, size: int):
         self.colomn = size
         self.row = size
-    
-    def load_board(self, filename):
         board = [["0" for i in range(self.colomn)] for j in range(self.row)]
-        board = np.array(board)
-        
+        self.board = np.array(board)
+    
+    def load_board(self, filename: str):  
+
         with open(filename, 'r') as f:
             variablearray = []
             for line in f:
@@ -23,21 +23,28 @@ class Board():
             variablearray.pop(0)
         for i in variablearray:
             if i[1] == "H":
-                board[int(i[3]) - 1][int(i[2]) - 1] = i[0]
+                self.board[int(i[3]) - 1][int(i[2]) - 1] = i[0]
                 if i[4] == "2":
-                    board[int(i[3]) - 1][int(i[2])] = i[0]
+                    self.board[int(i[3]) - 1][int(i[2])] = i[0]
                 if i[4] == "3":
-                    board[int(i[3]) - 1][int(i[2])] = i[0]
-                    board[int(i[3]) - 1][int(i[2]) + 1] = i[0]
+                    self.board[int(i[3]) - 1][int(i[2])] = i[0]
+                    self.board[int(i[3]) - 1][int(i[2]) + 1] = i[0]
             else:
-                board[int(i[3]) - 1][int(i[2]) - 1]= i[0]
+                self.board[int(i[3]) - 1][int(i[2]) - 1]= i[0]
                 if i[4] == "2":
-                    board[int(i[3])][int(i[2]) - 1] = i[0]
+                    self.board[int(i[3])][int(i[2]) - 1] = i[0]
                 if i[4] == "3":
-                    board[int(i[3])][int(i[2]) - 1] = i[0]
-                    board[int(i[3]) + 1][int(i[2]) - 1] = i[0]
-        print(board)
+                    self.board[int(i[3])][int(i[2]) - 1] = i[0]
+                    self.board[int(i[3]) + 1][int(i[2]) - 1] = i[0]
+        print(self.board)
+    
+    def move(self):
+
+
+
 
 test =  Board(6)
-print(test.load_board("Rushhour6x6_1.csv"))
+test.load_board("Rushhour6x6_1.csv")
+# print(test.load_board("Rushhour6x6_1.csv"))
+
 
