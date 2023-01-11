f0921311458dfe4b2e6aa80ab15f00e1108932ae
Alex van Diepen <alexv@HPalex2022.localdomain>
2023-01-11 13:22:37 +0100

    car.py toevoegen

---
+++ car.py
@@ -0,0 +1,16 @@
+import numpy as np 
+
+class Car:
+
+    def __init__(self, name, orientation, column, row, length):
+        self.name = name 
+        self.orientation = orientation 
+        self.column = column 
+        self.row = row 
+        self.length = length 
+        
+
+    def move(self):
+
+        if self.orientation == 'H':
+            
