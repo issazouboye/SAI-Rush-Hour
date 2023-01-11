ca336d2414bc923710653e76eaabb9a4409c758a
issazouboye <issazouboye@gmail.com>
2023-01-11 15:26:18 +0100

    begin grid

--- board.py
+++ board.py
@@ -10,18 +10,29 @@ class Board():
         self.colomn = size
         self.row = size
     
-    def load_board(filename):
+    def load_board(self, filename):
+        board = [["0" for i in range(self.colomn)] for j in range(self.row)]
+        board = np.array(board)
         
-            
+        with open(filename, 'r') as f:
+            variablearray = []
+            for line in f:
+                line = line.strip()
+                variables = line.split(",")
+                variablearray.append(variables)
+            variablearray.pop(0)
+        for i in variablearray:
+            if i[1] == "H":
+                board[int(i[3]) - 1][int(i[2]) - 1] = i[0]
+                if i[4] == "2":
+                    board[int(i[3]) - 1][[int(i[2])]] = i[0]
+                if i[4] == "3":
+                    board[int(i[3]) - 1][[int(i[2])]] = i[0]
+                    board[int(i[3]) - 1][[int(i[2]) + 1]] = i[0]
+            else:
+                board[int(i[3]) - 1][int(i[2]) - 1]= i[0]
+        print(board)
 
-
-with open("Rushhour6x6_1.csv", 'r') as f:
-    variablearray = []
-    for line in f:
-        line = line.strip()
-        variables = line.split(",")
-        variablearray.append(variables)
-    variablearray.pop(0)
-    print(variablearray)
-
+test =  Board(6)
+print(test.load_board("Rushhour6x6_1.csv"))
 
