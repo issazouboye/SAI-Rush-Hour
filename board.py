fa282611b280b582a05d34ce4bb535b5f92c7b80
issazouboye <issazouboye@gmail.com>
2023-01-13 12:21:49 +0100

    output list

--- board.py
+++ board.py
@@ -26,19 +26,20 @@ class Board():
                 self.board[int(i[3]) - 1][int(i[2]) - 1] = i[0]
                 if i[4] == "2":
                     self.board[int(i[3]) - 1][int(i[2])] = i[0]
-                if i[4] == "3":
+                elif i[4] == "3":
                     self.board[int(i[3]) - 1][int(i[2])] = i[0]
                     self.board[int(i[3]) - 1][int(i[2]) + 1] = i[0]
             else:
                 self.board[int(i[3]) - 1][int(i[2]) - 1]= i[0]
                 if i[4] == "2":
                     self.board[int(i[3])][int(i[2]) - 1] = i[0]
-                if i[4] == "3":
+                elif i[4] == "3":
                     self.board[int(i[3])][int(i[2]) - 1] = i[0]
                     self.board[int(i[3]) + 1][int(i[2]) - 1] = i[0]
         print(self.board)
     
     def move(self):
+        outputlist = []
         for i in range(len(self.board)):
             for j in range(len(self.board)):
                 if self.board[i][j] != "0":
@@ -47,36 +48,44 @@ class Board():
                             if j-1>=0 and self.board[i][j-1] == "0":
                                 self.board[i][j-1] = self.board[i][j]
                                 self.board[i][j+1] = "0"
+                                outputlist.append((self.board[i][j], -1))
                             elif j+1 < self.row and self.board[i][j+1] == "0":
                                 self.board[i][j+1] = self.board[i][j]
                                 self.board[i][j-1] = "0"
+                                outputlist.append((self.board[i][j], 1))
                         else:
                             if j-1 >= 0 and self.board[i][j-1] == "0":
                                 self.board[i][j-1] = self.board[i][j]
                                 self.board[i][j+2] = "0"
+                                outputlist.append((self.board[i][j], -1))
                             elif j+1< self.row and self.board[i][j+1] == "0":
                                 self.board[i][j+1] = self.board[i][j]
                                 self.board[i][j-2] = "0"
+                                outputlist.append((self.board[i][j], 1))
                     else:
                         if self.cars[self.board[i][j]].length == "2":
                             # verticale move naar beneden
                             if i+1< self.row and self.board[i + 1][j] == "0":
                                 self.board[i + 1][j] = self.board[i][j]
                                 self.board[i - 1][j] = "0"
+                                outputlist.append((self.board[i][j], 1))
                             elif i-1 >= 0 and self.board[i-1][j] == "0":
                                 self.board[i-1][j] = self.board[i][j]
                                 self.board[i+1][j] = "0"
-                                print(self.board)
+                                outputlist.append((self.board[i][j], -1))
                         else:
                             if i-1 >= 0 and self.board[i-1][j] == "0":
                                 self.board[i-1][j] = self.board[i][j]
                                 self.board[i+2][j] = "0"
+                                outputlist.append((self.board[i][j], -1))
                             elif i+1< self.row and self.board[i+1][j] == "0":
                                 self.board[i+1][j] = self.board[i][j]
                                 self.board[i-2][j] = "0"
+                                outputlist.append((self.board[i][j], 1))
 
 
         print(self.board)
+        print(outputlist)
 
 
 
@@ -85,9 +94,8 @@ class Board():
 
 
 test =  Board(6)
-test.load_board("Rushhour6x6_1.csv")
-for i in range(2):
-    test.move()
-# print(test.load_board("Rushhour6x6_1.csv"))
+test.load_board("Rushhour6x6_3.csv")
+test.move()
+# print(test.load_board("Rushhour9x9_6.csv"))
 
 
