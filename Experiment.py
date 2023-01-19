6f20ca3378a9775649eb38cbe3e4fd1e9d6baae5
Shrey1810 <shreyas.potdar@gmail.com>
2023-01-19 11:47:34 +0100

    Experiment

--- Experiment.py
+++ Experiment.py
@@ -1,5 +1,5 @@
 
-from board_v2 import *
+from board_random import *
 import matplotlib.pyplot as plt
 
 class Experiment:
@@ -8,30 +8,46 @@ class Experiment:
         self.step_list = []
 
     def experiment_list(self):
-        for i in range(self.runs):
-            initial_board = Board(6)
-            initial_board.load_board("Rushhour6x6_1.csv")
 
-            initial_cars = initial_board.get_initial_cars() 
-            initial_board = initial_board.get_initial_board() 
+        smallest_steps = 100000
 
-            random_solver = Random_solver(initial_cars, initial_board) 
+        initial_board = Board(6)
+        initial_board.load_board("Rushhour6x6_1.csv")     
+
+        initial_cars = initial_board.get_initial_cars() 
+        initial_board = initial_board.get_initial_board()  
+
+        master_random_solver = Random_solver_v2(initial_cars, initial_board)  
+    
+        for i in range(self.runs):     
+
+            random_solver = copy.deepcopy(master_random_solver)
             random_solver.solve_board()
-            
-            self.step_list.append(random_solver.step_count())
+
+            end_cars = random_solver.get_end_cars()
+            end_board = random_solver.get_end_board()
+            steps = random_solver.step_count()
+
+            self.step_list.append(steps)
+
+            if steps < smallest_steps:
+                smallest_steps = steps                          
+
+        print()
+        print(f"The smallest number of steps is: {smallest_steps}") 
+
 
 
     def plot(self):
-        plt.hist(self.step_list, edgecolor='black')
+        bins=np.arange(0, 100000, 1000)
+        plt.hist(self.step_list, edgecolor='black', bins = bins)
+        plt.title('Aantal Stappen om Rush Hour op te lossen')
         plt.xlabel('Amount of Steps')
         plt.ylabel('Frequencey')
-        plt.savefig('Experiment.png')
-
-
-
+        plt.savefig('Experiment_1000_runs.png')
 
 if __name__ == "__main__":
 
-    new_experiment = Experiment(100)
+    new_experiment = Experiment(1000)
     new_experiment.experiment_list()
     new_experiment.plot()
