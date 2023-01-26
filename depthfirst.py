4784f42fe29eb8d6cc633e02c79a84534efcd6a3
Shrey1810 <shreyas.potdar@gmail.com>
2023-01-26 15:14:10 +0100

    depth first clean up

--- depthfirst.py
+++ depthfirst.py
@@ -1,142 +1,11 @@
 # Depth First Search Algorithm
-from boardtest import *
-from visualization import visualize
+from __future__ import annotations
+# from visualize import visualize
 from car import Car
+from board import Board
+from state import State
 import copy 
 
-
-class Board:
-
-    def __init__(self, size: int):
-        self.size = size        
-        board = [["0" for i in range(self.size)] for j in range(self.size)]
-        self.board = np.array(board)
-
-        # Set with cars 
-        self.cars = set()        
-    
-    def load_board(self, filename: str):  
-
-        # Read the data
-        with open(filename, 'r') as f:
-            next(f) 
-
-            for line in f:
-                splits = line.strip().split(",")
-
-                name = splits[0] 
-                orientation = splits[1] 
-                column = int(splits[2]) - 1
-                row = int(splits[3]) - 1
-                length = int(splits[4])  
-
-                car = Car(name, orientation, column, row, length) 
-                self.cars.add(car)                                  
-        
-        # Place the cars on the board 
-        for car in self.cars:
-            if car.orientation == "H":
-                for j in range(car.length):
-                    self.board[car.row][car.column + j] = car.name 
-
-            if car.orientation == "V":
-                for i in range(car.length):
-                    self.board[car.row + i][car.column] = car.name 
-
-        print("First board:")
-        print(self.board)
-        print() 
-        # print(len(self.cars))
-
-    def get_initial_cars(self):
-        return self.cars 
-
-    def get_initial_board(self):
-        return self.board 
-    
-class State: 
-
-    def __init__(self, cars, size: int):
-        self.cars = cars
-        self.size = size  
-        self.board = None 
-        self.create_board()          
-
-    def create_board(self):
-        board = [["0" for i in range(self.size)] for j in range(self.size)]
-        self.board = np.array(board)
-
-        # Place the cars on the board 
-        for car in self.cars:
-            if car.orientation == "H":
-                for j in range(car.length):
-                    self.board[car.row][car.column + j] = car.name 
-
-            if car.orientation == "V":
-                for i in range(car.length):
-                    self.board[car.row + i][car.column] = car.name 
-        
-        return self.board     
-
-    def get_next_configurations(self): 
-
-        # List filled with sets of car objects 
-        configurations = []         
-
-        for car in self.cars:
-
-            # Check for horizontal cars 
-            if car.orientation == "H":
-
-                # Check if you can move to the left 
-                if car.is_movable("left", self.board):                     
-                    car.move_left() 
-                    configurations.append(copy.deepcopy(self.cars)) 
-                    car.move_right()                 
-
-                # Check if you can move to the right 
-                if car.is_movable("right", self.board):                     
-                    car.move_right() 
-                    configurations.append(copy.deepcopy(self.cars)) 
-                    car.move_left()                 
-
-            # Check for vertical cars 
-            if car.orientation == "V":
-
-                # Check if you can move up 
-                if car.is_movable("up", self.board):                    
-                    car.move_up()                      
-                    configurations.append(copy.deepcopy(self.cars)) 
-                    car.move_down()                    
-
-                # Check if you can move down 
-                if car.is_movable("down", self.board):                    
-                    car.move_down() 
-                    configurations.append(copy.deepcopy(self.cars)) 
-                    car.move_up()                    
-
-        return configurations          
-
-    def is_solved(self): 
-        for car in self.cars:
-            winning_column = self.size - 2 
-            winning_row = ceil(self.size / 2) - 1 
-
-            if car.name == "X" and car.column == winning_column and car.row == winning_row:
-                return True 
-
-        return False  
-    
-    def __hash__(self) -> int:
-        return hash(self.__repr__())
-    
-    def __repr__(self) -> str:
-        printable_board = np.array_str(self.board)
-
-        return printable_board 
-
-    def __eq__(self, other) -> bool:
-        return isinstance(other, State) 
 
 class Depthfirst:
     def __init__(self, cars_set: State, size: int, max_height: int):
@@ -161,7 +30,6 @@ class Depthfirst:
 
          while len(self.stack) != 0:
             new_state = self.stack.pop()
-            self.steps += 1
 
             # if self.archieve[new_state][1] > self.max_height:
             if new_state.is_solved():
